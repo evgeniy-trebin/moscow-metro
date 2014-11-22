e184e8d0ca9f91dfe6083c152e66e7f642e7510d
Sergey Bronnikov <estetus@gmail.com>
2014-11-23 01:20:21 +0300

    Fixed some bugs in calculate, fixed and added new stations to stations-coord.json

--- calculate.py
+++ calculate.py
@@ -11,7 +11,7 @@ except ImportError:
 from collections import defaultdict
 
 json_time_source="moscow-metro.json"
-json_distance_source="stations.json"
+json_coord_source="stations-coord.json"
 
 def calculate():
         json_data=open(json_time_source).read()
@@ -21,35 +21,49 @@ def calculate():
         for l in range(1, links):
             fromStation = time_data[0]["links"][str(l)]["fromStationId"]
             toStation = time_data[0]["links"][str(l)]["toStationId"]
-            line = time_data[0]["stations"][str(fromStation)]["lineId"]
-            linename = time_data[0]["lines"][str(line)]["name"]
+            toline = time_data[0]["stations"][str(toStation)]["lineId"]
+            fromline = time_data[0]["stations"][str(fromStation)]["lineId"]
+            tolinename = time_data[0]["lines"][str(toline)]["name"]
+            fromlinename = time_data[0]["lines"][str(fromline)]["name"]
             nameFromStation = time_data[0]["stations"][str(fromStation)]["name"]
             nameToStation = time_data[0]["stations"][str(toStation)]["name"]
             time = time_data[0]["links"][str(l)]["weightTime"]
 
-            tlat = get_coord(linename, nameFromStation, "lat")
-            tlon = get_coord(linename, nameFromStation, "long")
-            flat = get_coord(linename, nameToStation, "lat")
-            flon = get_coord(linename, nameToStation, "long")
-           
+            flat = get_coord(fromlinename, nameFromStation, "lat")
+            flon = get_coord(fromlinename, nameFromStation, "long")
+            tlat = get_coord(tolinename, nameToStation, "lat")
+            tlon = get_coord(tolinename, nameToStation, "long")
+
+            #if not flat:
+            #   print nameFromStation, " - ", flat, fromlinename, fromline
+            #if not tlat:
+            #   print nameToStation, " - ", tlat, tolinename, toline
+            #print nameToStation, tlat, tlon
+
+            #print get_coord(u"Бутовская линия", u"Бульвар Адмирала Ушакова", "long")
+
             permalink = "http://www.yournavigation.org/?flat=" + flat + \
-            "&flon=" + flon + "&tlat=" + tlat + "&tlon=" + tlon + "&v=foot&shortest=1&layer=mapnik"
-            if (flat and flon and tlat and tlon):
-               #print "   ", flat, flon, tlat, tlon, nameFromStation, nameToStation
-               print "%s, %s, %s, %d, http://metro.yandex.ru/moscow?from=%d&to=%d&route=0, %s, %s" % (linename, nameFromStation, nameToStation, time, fromStation, toStation, get_distance(flat, flon, tlat, tlon), permalink)
+            "flon=" + flon + "&tlat=" + tlat + "&tlon=" + tlon + "&v=foot&shortest=1&layer=mapnik"
+
+            #print "   ", flat, flon, tlat, tlon, nameFromStation, nameToStation
+            print "%s, %s, %s, %s, %d, http://metro.yandex.ru/moscow?from=%d&to=%d&route=0, %s, %s" % (fromlinename, tolinename, nameFromStation, nameToStation, time, fromStation, toStation, get_distance(flat, flon, tlat, tlon), permalink)
 
 def get_coord(lnname, stname, coordinate):
-        json_data=open(json_distance_source).read()
-        distance_data = json.loads(json_data)
-
-        for jline in distance_data:
+        json_data=open(json_coord_source).read()
+        coord_data = json.loads(json_data)
+        #print "=====", lnname, stname
+        for jline in coord_data:
             if jline["line"] == lnname:
+               #print jline["line"], lnname
                for station in jline["stations"]:
                    if station["station"] == stname:
                       return station[coordinate]
+                      #return station["station"]
 
 def get_coord1(lname, sname, coordinate):
-        url = "http://geocode-maps.yandex.ru/1.x/?&geocode=" + sname + "&kind=metro&format=json&rspn=1&ll=37.618920,55.756994&spn=0.552069,0.400552"
+        url = "http://geocode-maps.yandex.ru/1.x/?&geocode=" + lname + "," + sname + "&kind=metro&format=json"
+        #json_coord = json.load(urllib2.urlopen(url))
+        #print json_coord["response"]["GeoObjectCollection"]
 
 def get_distance(flat, flon, tlat, tlon):
         url = "http://www.yournavigation.org/api/1.0/gosmore.php?format=geojson&flat=" \
